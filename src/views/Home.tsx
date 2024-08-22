c2e8ef88d535026a8d71032d19e5df641525033d
Ramzan <135956054+MhRamzan21@users.noreply.github.com>
2024-08-22 22:43:39 +0500

    Update Home.tsx

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -3,7 +3,9 @@ type Props = {};
 function Home({}: Props) {
   return (
     <div className="my-10 flex-1">
+      <h4 className="text-blue-600 text-4xl font-semibold text-center">
         Home
+      </h4>
     </div>
   );
 }
